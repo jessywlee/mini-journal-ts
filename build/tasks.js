356943f84af20015b946f59653e6d4cfa39bef73
Jessica Lee <to.jessywlee@gmail.com>
2021-12-26 21:56:37 +0900

    [add] add task item by clicking add button and save data in local storage

--- build/tasks.js
+++ build/tasks.js
@@ -1,2 +1,35 @@
 "use strict";
+const taskArray = [];
+const taskList = document.querySelector('.task-list');
+const addTaskButton = document.querySelector('.add-button');
+function addTask(e) {
+    e.preventDefault();
+    const task = {
+        task: "",
+        id: Math.floor(Math.random() * 10000)
+    };
+    taskArray.push(task);
+    localStorage.setItem('tasks', JSON.stringify(taskArray));
+    //add DOM element
+    const taskCheckBox = document.createElement('input');
+    const taskDeleteButton = document.createElement('button');
+    const taskDeleteIcon = document.createElement('i');
+    const taskInput = document.createElement('input');
+    const taskItem = document.createElement('li');
+    taskCheckBox.type = 'checkbox';
+    taskCheckBox.className = 'task-checkbox';
+    taskDeleteButton.className = 'task-delete-btn';
+    taskDeleteIcon.className = 'far fa-trash-alt fa-lg';
+    taskInput.type = 'text';
+    taskInput.className = 'task-input';
+    taskItem.className = 'task-item';
+    taskDeleteButton.appendChild(taskDeleteIcon);
+    taskItem.appendChild(taskCheckBox);
+    taskItem.appendChild(taskInput);
+    taskItem.appendChild(taskDeleteButton);
+    taskList === null || taskList === void 0 ? void 0 : taskList.appendChild(taskItem);
+}
+function writeTask() {
+}
+addTaskButton === null || addTaskButton === void 0 ? void 0 : addTaskButton.addEventListener('click', (e) => { addTask(e); });
 //# sourceMappingURL=tasks.js.map
--- src/tasks.ts
+++ src/tasks.ts
@@ -1,4 +1,45 @@
-type taskList = {
+type Task = {
 	task: string;
 	id: number;
 };
+
+const taskArray: Array<object> = [];
+const taskList = document.querySelector('.task-list');
+const addTaskButton = document.querySelector('.add-button');
+
+function addTask (e:Event): void {
+	e.preventDefault();
+	const task: Task = {
+	task: "",
+	id: Math.floor(Math.random() * 10000)
+	}
+	taskArray.push(task);
+	localStorage.setItem('tasks', JSON.stringify(taskArray))
+	
+	//add DOM element
+	const taskCheckBox = document.createElement('input');
+	const taskDeleteButton = document.createElement('button');
+	const taskDeleteIcon = document.createElement('i');
+	const taskInput = document.createElement('input')
+	const taskItem = document.createElement('li');
+
+	taskCheckBox.type = 'checkbox';
+	taskCheckBox.className = 'task-checkbox';
+	taskDeleteButton.className = 'task-delete-btn';
+	taskDeleteIcon.className = 'far fa-trash-alt fa-lg';
+	taskInput.type = 'text';
+	taskInput.className = 'task-input';
+	taskItem.className = 'task-item';
+
+	taskDeleteButton.appendChild(taskDeleteIcon);
+	taskItem.appendChild(taskCheckBox);
+	taskItem.appendChild(taskInput);
+	taskItem.appendChild(taskDeleteButton);
+	taskList?.appendChild(taskItem);
+
+}
+
+function writeTask() {
+
+}
+addTaskButton?.addEventListener('click', (e)=> {addTask(e)});
