254ad8fe71eb259578aa6818c489779a9bb1a539
Jessica Lee <to.jessywlee@gmail.com>
2021-12-27 01:13:23 +0900

    [add] delete task feature

--- build/tasks.js
+++ build/tasks.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const data = localStorage.getItem('tasks');
 let taskArray = [];
 if (data) {
@@ -7,27 +16,29 @@ if (data) {
 const taskList = document.querySelector('.task-list');
 const addTaskButton = document.querySelector('.add-button');
 function loadTask(tasks) {
-    const taskCheckBox = document.createElement('input');
-    const taskDeleteButton = document.createElement('button');
-    const taskDeleteIcon = document.createElement('i');
-    const taskInput = document.createElement('input');
-    const taskItem = document.createElement('li');
-    taskArray.forEach(task => {
+    tasks.forEach(task => {
+        const taskCheckBox = document.createElement('input');
+        const taskDeleteButton = document.createElement('button');
+        const taskDeleteIcon = document.createElement('i');
+        const taskInput = document.createElement('input');
+        const taskItem = document.createElement('li');
         taskCheckBox.type = 'checkbox';
         taskCheckBox.className = 'task-checkbox';
         taskDeleteButton.className = 'task-delete-btn';
         taskDeleteIcon.className = 'far fa-trash-alt fa-lg';
+        taskDeleteIcon.accessKey = task.id.toString();
         taskInput.type = 'text';
         taskInput.className = 'task-input';
         taskInput.id = task.id.toString();
         taskInput.value = task.task;
         taskItem.className = 'task-item';
-        taskItem.classList.add(task.id.toString());
+        taskItem.accessKey = task.id.toString();
         taskDeleteButton.appendChild(taskDeleteIcon);
         taskItem.appendChild(taskCheckBox);
         taskItem.appendChild(taskInput);
         taskItem.appendChild(taskDeleteButton);
         taskList === null || taskList === void 0 ? void 0 : taskList.appendChild(taskItem);
+        taskDeleteIcon.addEventListener('click', e => { deleteTask(e); });
     });
 }
 function addTask(e) {
@@ -48,11 +59,12 @@ function addTask(e) {
     taskCheckBox.className = 'task-checkbox';
     taskDeleteButton.className = 'task-delete-btn';
     taskDeleteIcon.className = 'far fa-trash-alt fa-lg';
+    taskDeleteIcon.accessKey = task.id.toString();
     taskInput.type = 'text';
     taskInput.className = 'task-input';
     taskInput.id = task.id.toString();
     taskItem.className = 'task-item';
-    taskItem.classList.add(task.id.toString());
+    taskItem.accessKey = task.id.toString();
     taskDeleteButton.appendChild(taskDeleteIcon);
     taskItem.appendChild(taskCheckBox);
     taskItem.appendChild(taskInput);
@@ -69,6 +81,19 @@ function writeTask(e) {
         }
     });
     localStorage.setItem('tasks', JSON.stringify(taskArray));
+}
+function deleteTask(e) {
+    return __awaiter(this, void 0, void 0, function* () {
+        // e.preventDefault();
+        const target = e.target;
+        const taskId = target.accessKey;
+        yield taskArray.forEach((task, index) => {
+            if (task.id === Number(taskId)) {
+                taskArray.splice(index, 1);
+                localStorage.setItem('tasks', JSON.stringify(taskArray));
+            }
+        });
+    });
 }
 loadTask(taskArray);
 addTaskButton === null || addTaskButton === void 0 ? void 0 : addTaskButton.addEventListener('click', (e) => { addTask(e); });
--- src/tasks.ts
+++ src/tasks.ts
@@ -13,30 +13,35 @@ const taskList = document.querySelector('.task-list');
 const addTaskButton = document.querySelector('.add-button');
 
 function loadTask (tasks: Array<Task>): void {
-	const taskCheckBox = document.createElement('input');
-	const taskDeleteButton = document.createElement('button');
-	const taskDeleteIcon = document.createElement('i');
-	const taskInput = document.createElement('input')
-	const taskItem = document.createElement('li');
 
-	taskArray.forEach(task => {
+	tasks.forEach(task => {
+		const taskCheckBox = document.createElement('input');
+		const taskDeleteButton = document.createElement('button');
+		const taskDeleteIcon = document.createElement('i');
+		const taskInput = document.createElement('input')
+		const taskItem = document.createElement('li');
+
 		taskCheckBox.type = 'checkbox';
 		taskCheckBox.className = 'task-checkbox';
 		taskDeleteButton.className = 'task-delete-btn';
 		taskDeleteIcon.className = 'far fa-trash-alt fa-lg';
+		taskDeleteIcon.accessKey = task.id.toString();
 		taskInput.type = 'text';
 		taskInput.className = 'task-input';
 		taskInput.id = task.id.toString();
 		taskInput.value = task.task;
 		taskItem.className = 'task-item';
-		taskItem.classList.add(task.id.toString());
+		taskItem.accessKey = task.id.toString();
 
 		taskDeleteButton.appendChild(taskDeleteIcon);
 		taskItem.appendChild(taskCheckBox);
 		taskItem.appendChild(taskInput);
 		taskItem.appendChild(taskDeleteButton);
 		taskList?.appendChild(taskItem);
+
+		taskDeleteIcon.addEventListener('click', e => {deleteTask(e)})
 	})
+	
 	
 }
 
@@ -60,11 +65,12 @@ function addTask (e:Event): void {
 	taskCheckBox.className = 'task-checkbox';
 	taskDeleteButton.className = 'task-delete-btn';
 	taskDeleteIcon.className = 'far fa-trash-alt fa-lg';
+	taskDeleteIcon.accessKey = task.id.toString();
 	taskInput.type = 'text';
 	taskInput.className = 'task-input';
 	taskInput.id = task.id.toString();
 	taskItem.className = 'task-item';
-	taskItem.classList.add(task.id.toString());
+	taskItem.accessKey = task.id.toString();
 
 	taskDeleteButton.appendChild(taskDeleteIcon);
 	taskItem.appendChild(taskCheckBox);
@@ -86,6 +92,20 @@ function writeTask(e: Event): void {
 
 	localStorage.setItem('tasks', JSON.stringify(taskArray));
 }
+
+async function deleteTask(e: Event): Promise<void> {
+	// e.preventDefault();
+	const target = e.target as HTMLButtonElement;
+	const taskId = target.accessKey;
+
+	await taskArray.forEach((task, index) => {
+		if (task.id === Number(taskId)) {
+			taskArray.splice(index, 1)
+			localStorage.setItem('tasks', JSON.stringify(taskArray))
+		}
+	})
+}
+
 loadTask(taskArray);
 addTaskButton?.addEventListener('click', (e)=> {addTask(e)});
 taskList?.addEventListener('change', (e) => {writeTask(e)});
