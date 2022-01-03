c1e0068ab08ade9a46927bc2b0a4a5c4b5d61f05
yewon.lee <yewon.lee@seavantage.com>
2022-01-03 16:20:39 +0900

    [add] tasks

--- build/tasks.js
+++ build/tasks.js
@@ -1,2 +1,26 @@
 "use strict";
+const taskArray = [];
+const addTaskButton = document.querySelector(".add-button");
+const taskList = document.querySelector(".task-list");
+const taskItems = document.querySelectorAll(".task-item");
+addTaskButton === null || addTaskButton === void 0 ? void 0 : addTaskButton.addEventListener("click", (e) => {
+    addTask(e);
+});
+function addTask(e) {
+    e.preventDefault();
+    const item = document.createElement("li");
+    const input = document.createElement("input");
+    const button = document.createElement("button");
+    const icon = document.createElement("i");
+    icon.className = "far fa-trash-alt fa-lg";
+    button.className = "task-delete-btn";
+    input.className = "task-input";
+    item.className = "task-item";
+    button.appendChild(icon);
+    taskList === null || taskList === void 0 ? void 0 : taskList.appendChild(item);
+}
+const task = {
+    task: "hello",
+    id: 1,
+};
 //# sourceMappingURL=tasks.js.map
--- src/tasks.ts
+++ src/tasks.ts
@@ -1,4 +1,32 @@
-type taskList = {
+const taskArray: Array<object> = [];
+type Task = {
 	task: string;
 	id: number;
+};
+const addTaskButton = document.querySelector(".add-button");
+const taskList = document.querySelector(".task-list");
+const taskItems = document.querySelectorAll(".task-item");
+
+addTaskButton?.addEventListener("click", (e) => {
+	addTask(e);
+});
+
+function addTask(e: Event): void {
+	e.preventDefault();
+	const item = document.createElement("li");
+	const input = document.createElement("input");
+	const button = document.createElement("button");
+	const icon = document.createElement("i");
+	icon.className = "far fa-trash-alt fa-lg";
+	button.className = "task-delete-btn";
+	input.className = "task-input";
+	item.className = "task-item";
+	button.appendChild(icon);
+
+	taskList?.appendChild(item);
+}
+
+const task: Task = {
+	task: "hello",
+	id: 1,
 };
